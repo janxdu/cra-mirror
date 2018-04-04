841cad149d3cb8e1eb7905f0e221411c065e0318
sailingcai <sailingcai@gmail.com>
2018-04-04 16:17:19 +0800

    init

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import { Button, Icon } from 'antd';
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home1</h2>
+    <Button type="primary">Button5</Button>
+    <Icon type="link" />
+  </div>
+);
+
+export default Home;
