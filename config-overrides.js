b1bcaad3a87e3af76e33983afd51b6cd360ba317
sailingcai <sailingcai@gmail.com>
2018-04-04 16:16:33 +0800

    init

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,17 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);
+  config = rewireLess(config, env);
+  // with loaderOptions
+  config = rewireLess.withLoaderOptions({
+    javascriptEnabled: true,
+    modifyVars: {
+      'primary-color': '#1DA57A',
+      'link-color': '#1DA57A',
+      'border-radius-base': '2px',
+    },
+  })(config, env);
   return config;
 };
