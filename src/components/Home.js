39436f02cb63dee137969fb131549207a3918b17
sailingcai <sailingcai@gmail.com>
2018-04-08 23:10:51 +0800

    init

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
-import { Button, Icon } from 'antd';
+import { Button } from 'antd';
 import React from 'react';
+import { IconTip, PasswordInput, UsernameInput } from 'kx-components';
+import 'kx-components/index.css';
 
 const Home = () => (
   <div>
-    <h2>Home1</h2>
+    <h2>Home</h2>
     <Button type="primary">Button5</Button>
-    <Icon type="link" />
+    <PasswordInput placeholder="请输入密码" value="12345678" size="large" />
+    <UsernameInput placeholder="请输入用户名" value="12345678" size="large" />
+    <IconTip icon="user" tip="这里一段描述" style={{ fontSize: '30px' }} />
   </div>
 );
 
