019aeebd792d06d417dc4e311eabe248284a92f5
sailingcai <sailingcai@gmail.com>
2018-04-30 20:31:36 +0800

    添加介绍

--- src/page/IntroductionPage.jsx
+++ src/page/IntroductionPage.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 const IntroductionPage = () => (
-  <div>Hello KX React!</div>
+  <div>
+    这个项目将会介绍向前冲这个项目利用React到底干了些什么。
+  </div>
 );
 
 export default IntroductionPage;
